5ee0aac7fefb21fbec110ba9409011e8edb58c4b
Bishvesh Tripathi <rajtripathi54671@gmail.com>
2023-12-20 14:24:24 +0530

    Wrapped up my News App by implementing loding indicator

--- App/Screen/Home.js
+++ App/Screen/Home.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, ActivityIndicator, Dimensions } from 'react-native'
 import React, { useState,useEffect, } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import GlobalApi from '../Services/GlobalApi'
@@ -10,25 +10,33 @@ import Color from '../Shared/Color';
 const Home = () => {
 
     const[newsList,setNewsList]=useState([]);
+    const[loading,setLoading]=useState(true);
     useEffect(()=>{
         // getTopHeadline();
         getNewsByCategory('latest');
     },[])
 
     const getNewsByCategory=async(category)=>{
+        setLoading(true);
         const result=(await GlobalApi.getByCategory(category)).data;
         setNewsList(result.articles);
+        setLoading(false);
     }
 
   return (
     <ScrollView style={{backgroundColor:Color.white}}>
-        <View style={{display:'flex',flexDirection:"row",justifyContent:'space-between',alignItems:'center'}}>
+     
+          <View style={{display:'flex',flexDirection:"row",justifyContent:'space-between',alignItems:'center'}}>
           <Text style={styles.appName}>Todays NEWS</Text>
           <Ionicons name="notifications-outline" size={28} color="black" />
         </View>
         <CategoryTextSlider selectCategory={(category)=>getNewsByCategory(category)}/>
+        {loading?<ActivityIndicator size={'large'} color={Color.primary} style={{marginTop:Dimensions.get('screen').height*0.40}}/>: 
+        <View>
         <TopHeadlineSlider newsList={newsList}/>
         <HeadlineList newsList={newsList}/>
+        </View>
+      }
     </ScrollView>
   )
 }
