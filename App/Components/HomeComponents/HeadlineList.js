4e6e6e3ee334e08f86f3c1553d08f85dd19a3fc2
Bishvesh Tripathi <rajtripathi54671@gmail.com>
2023-12-20 13:42:06 +0530

    Implemented back/share button & In app browsing on Read More button

--- App/Components/HomeComponents/HeadlineList.js
+++ App/Components/HomeComponents/HeadlineList.js
@@ -1,8 +1,10 @@
 import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import Color from '../../Shared/Color'
+import { useNavigation } from '@react-navigation/native';
 
 const HeadlineList = ({newsList}) => {
+  const navigation=useNavigation();
   return (
     <View>
         <FlatList
@@ -11,10 +13,10 @@ const HeadlineList = ({newsList}) => {
         renderItem={({item})=>(
            <View>
             <View style={{height:1,backgroundColor:Color.lightGray,marginTop:15}}></View>
-             <TouchableOpacity style={{marginTop:15,display:'flex',flexDirection:'row',gap:10}}>
-                <Image source={{uri:item.urlToImage}} style={{width:130,height:130,borderRadius:10}}/>
+             <TouchableOpacity onPress={()=>navigation.navigate('read-news',{news:item})} style={{marginTop:15,display:'flex',flexDirection:'row',gap:10}}>
+                <Image source={{uri:item?.urlToImage}} style={{width:130,height:130,borderRadius:10}}/>
                 <View style={{marginRight:130}}>
-                 <Text numberOfLines={4} style={{fontSize:18,fontWeight:'bold'}}>{item.title}</Text>
+                 <Text numberOfLines={4} style={{fontSize:18,fontWeight:'bold'}}>{item?.title}</Text>
                  <Text style={{color:Color.primary,marginTop:7}}>{item?.source?.name}</Text>
                 </View>
             </TouchableOpacity>
--- App/Screen/ReadNews.js
+++ App/Screen/ReadNews.js
@@ -1,20 +1,42 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Share } from 'react-native'
 import React, { useEffect } from 'react'
-import { useRoute } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import Color from '../Shared/Color';
+import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import * as WebBrowser from 'expo-web-browser';
+
 
 const ReadNews = () => {
     const news=useRoute().params.news;
+    const navigation=useNavigation();
     useEffect(()=>{
         console.log(news);
     },[])
+    const shareNews=()=>{
+      Share.share({
+        message:news.title+"\n Read More"+news.description
+      })
+    }
   return (
-    <View style={{backgroundColor:Color.white}}>
+    <ScrollView style={{backgroundColor:Color.white,flex:1}}>
+      <View style={{marginTop:13,marginBottom:13,display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+        <TouchableOpacity onPress={()=>navigation.goBack()}>
+        <Ionicons name="arrow-back" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>shareNews()}>
+        <Entypo name="share" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
         <Image source={{uri:news.urlToImage}} style={{width:'100%',height:300,borderRadius:15}}/>
         <Text style={{marginTop:10,fontSize:22,fontWeight:'bold'}}>{news.title}</Text>
         <Text style={{marginTop:10,fontSize:16,color:Color.primary}}>{news.source.name}</Text>
         <Text style={{marginTop:10,fontSize:18,color:Color.gray,lineHeight:30}}>{news.description}</Text>
-    </View>
+        <TouchableOpacity onPress={()=>WebBrowser.openBrowserAsync(news.url)}>
+        <Text style={{marginTop:10,color:Color.primary,fontSize:16,fontWeight:'bold'}}>Read More</Text>
+        </TouchableOpacity>
+    </ScrollView>
   )
 }
 
